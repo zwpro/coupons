c67891113e5192d69e870f8ae3bef8962bc79149
zhenhuaixiu <huaixiu.zhen@gmail.com>
2021-02-28 00:13:56 +0800

    optimize

---
+++ pages/about/about.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+	  <view class="about">
+        敬广大干饭组用户：
+        <view>1.欢迎您使用干饭组小程序，干饭组小程序旨为广大用户提供每日领取外卖红包的快捷方式。</view>
+        <view>2.其中第三方外卖平台皆为官方小程序平台，本项目不会未经允许收集您的任何私人信息。</view>
+        <view>3.首次使用外卖平台小程序领取红包需授权微信登陆，领取的红包可在该平台下的任何客户端查询与使用，建议您添加到“我的小程序”中，方便每次领取。</view>
+        <view>4.干饭组会持续为广大用户提供每日外卖红包、优质商品推荐等内容。</view>
+        <view>5.最后感谢每位小可爱的支持，不积跬步，无以至千里，愿与君共勉，冲啊，干饭人！</view>
+      </view>
+	</view>
+</template>
+
+<style lang="scss">
+page {
+	background-color: #f8f8f8;
+}
+.container {
+	font-size: 14px;
+	line-height: 24px;
+	position: relative;
+	.about {
+        padding: 20rpx;
+	}
+}
+</style>
---
+++ pages/shop/shop.vue
@@ -0,0 +1,251 @@
+<template>
+	<view class="container">
+		<v-tabs v-model="current" :tabs="tabs" @change="changeTab" class="tab"></v-tabs>
+		<view class="coupon" ref="coupon">
+			<view class="item" v-for="(v, i) in couponList" @click="toCoupon(i)" :key="i">
+				<view class="top">
+					<view class="left">
+						<view class="content">
+							<image :src="v.icon" class="icon" mode="widthFix" />
+							<view class="name">{{ v.name }}</view>
+						</view>
+						<view class="text" v-if="v.type == 1">天天可领</view>
+						<view class="text" v-else-if="v.type == 2">限时秒杀</view>
+					</view>
+					<view class="right">免费领取</view>
+				</view>
+				<view class="bottom"><image :src="v.bannerPic" class="banner-image" mode="widthFix" /></view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			current: 0,
+			tabs: [
+				{
+					icon: '/static/all.png',
+					text: '全部',
+					tabId: 0,
+				},
+				{
+					icon: '/static/ele.png',
+					text: '饿了么',
+					tabId: 1,
+				},
+				{
+					icon: '/static/meituan.png',
+					text: '美团',
+					tabId: 2,
+				},
+				{
+					icon: '/static/tmall.png',
+					text: '天猫',
+					tabId: 5,
+				},
+				{
+					icon: '/static/jd.png',
+					text: '京东',
+					tabId: 3,
+				},
+				// {
+				// 	icon: '/static/vip.png',
+				// 	text: 'VIP会员',
+				// 	tabId: 4,
+				// }
+			],
+			couponList: [],
+			coupons: [
+				{
+					name: '饿了么红包',
+					icon: '/static/coupon/ele.png',
+					bannerPic: '/static/coupon/elm.jpg',
+					url: 'https://s.click.ele.me/quDa1ru',
+					type: 1,
+					tabId: 1,
+					minapp: {
+						appid: 'wxece3a9a4c82f58c9',
+						path: 'taoke/pages/shopping-guide/index?scene=kvnz0ru',
+					}
+				},
+				{
+					name: '美团外卖红包',
+					icon: '/static/coupon/meituan.png',
+					bannerPic: '/static/coupon/meituan_banner.png',
+					url:'https://runion.meituan.com/url?key=cd23768d09c339d1641b2738df39aa67&url=https%3A%2F%2Fi.meituan.com%2Fawp%2Fhfe%2Fblock%2Fa945391288b790d558b7%2F78716%2Findex.html%3Fappkey%3Dcd23768d09c339d1641b2738df39aa67%3Ajuhe&sid=juhe',
+					type: 1,
+					tabId: 2,
+					minapp: {
+						appid: 'wxde8ac0a21135c07d',
+						path: '/index/pages/h5/h5?noshare=1&f_userId=0&f_openId=0&f_token=0&weburl=https%3A%2F%2Fact1.meituan.com%2Fclover%2Fpage%2Fadunioncps%2Fshare_coupon_new%3FutmSource%3D2055%26timestamp%3D1614432427%26utmMedium%3D401f09cc5f627afb3fcc9e46f2ade88d%26version%3D1.0%26showKa%3D1%26requestId%3D97ad06f1617a19415603d95b6cd233eb%26activity%3DOwMkGzn6oK'
+					}
+				},
+			]
+		};
+	},
+	onLoad(e) {
+		//#ifdef H5
+		let tabId = this.$route.query.tabId ? parseInt(this.$route.query.tabId) : 0
+		//#endif
+		//#ifdef MP-WEIXIN
+		let tabId = e.tabId ? parseInt(e.tabId) : 0
+		//#endif
+		for(let i in this.tabs){
+			if(tabId == this.tabs[i].tabId){
+				this.current = parseInt(i)
+			}
+		}
+		this.changeTab(this.current)
+	},
+	onShareAppMessage(res) {
+		var messages = [{
+			title: '美团饿了么大额红包，每日可领！',
+			path: '/pages/index/index'
+		},{
+			title: '吃了这么多年外卖，你知道这个秘密吗？',
+			path: '/pages/index/index'
+		}];
+		return messages[Math.floor(Math.random()*messages.length)];
+	},
+	methods: {
+		changeTab(index) {
+			console.log('当前选中的项：' + index);
+			this.couponList = []
+			uni.showLoading({
+			    title: '获取优惠中'
+			});
+			if(index == 0){
+				this.couponList = this.coupons
+			}else{
+				for(let i in this.coupons){
+					if(this.coupons[i].tabId == this.tabs[index].tabId){
+						this.couponList.push(this.coupons[i])
+					}
+				}
+			}
+			//#ifdef H5
+			this.$nextTick(() => {
+				this.$refs.coupon.scrollTop= 0;
+			})
+			//#endif
+			setTimeout(() => {
+				uni.hideLoading()
+			}, 500)
+		},
+		toCoupon(i){
+			console.log(this.couponList[i])
+			//h5
+			//#ifdef H5
+			window.location.href = this.couponList[i].url
+			//#endif
+			//微信小程序
+			//#ifdef MP-WEIXIN
+			if(this.couponList[i].minapp){
+				wx.navigateToMiniProgram({
+				  appId: this.couponList[i].minapp.appid,
+				  path: this.couponList[i].minapp.path,
+				  success(res) {
+					// 打开成功
+				  }
+				})
+			}
+			//#endif
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+page {
+	background-color: #f8f8f8;
+}
+.container {
+	font-size: 14px;
+	line-height: 24px;
+	position: relative;
+	.tab {
+		position: fixed;
+		top: var(--window-top);
+		left: 0;
+		z-index: 9999;
+	}
+	.coupon {
+		padding-top: 200rpx;
+		.item {
+			background-color: #ffffff;
+			margin: 30rpx;
+			border-radius: 10rpx;
+			padding: 0 30rpx 30rpx 30rpx;
+			.top {
+				height: 116rpx;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				.left {
+					height: 116rpx;
+					width: 400rpx;
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					.content {
+						width: 100%;
+					}
+					.icon {
+						display: inline-block;
+						vertical-align: bottom;
+						width: 52rpx;
+						height: auto;
+					}
+					.name {
+						text-align: left;
+						display: inline-block;
+						vertical-align: bottom;
+						font-size: 34rpx;
+						color: #000;
+						line-height: 50rpx;
+						font-weight: bold;
+						margin-left: 15rpx;
+					}
+					.text {
+						width: 150rpx;
+						height: 38rpx;
+						line-height: 38rpx;
+						text-align: center;
+						font-size: 24rpx;
+						color: #61300e;
+						background: linear-gradient(90deg, #f9db8d, #f8d98a);
+						border-radius: 6rpx;
+					}
+				}
+
+				.right {
+					width: 170rpx;
+					height: 60rpx;
+					border-radius: 30rpx;
+					background: linear-gradient(90deg, #ec6f43, #ea4a36);
+					color: #fff;
+					font-size: 28rpx;
+					line-height: 60rpx;
+					text-align: center;
+				}
+			}
+
+			.bottom {
+				height: auto;
+				width: 100%;
+				image {
+					display: block;
+					width: 100%;
+					height: auto;
+				}
+			}
+			.banner-image {
+				border-radius: 20rpx;
+			}
+		}
+	}
+}
+</style>
